1e3770d9c56ad995bf5adab22058655cca1dc526
Daniel Mañá Guixà <daniel81bcn@gmail.com>
2016-02-03 16:10:10 -0600

    chore(title): update app title and description

--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'Known Error Database',
+    description: 'Driven by the IT community to rapidly categorize known patterns from events to determine risk',
+    keywords: 'events, patterns, severity, service, vendor, FCAPS, criticality, correlation',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
