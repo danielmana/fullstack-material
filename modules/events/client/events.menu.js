d966abe8936400c683c84823adc5a58515dfd9d4
Daniel Mañá Guixà <daniel81bcn@gmail.com>
2016-02-04 13:57:23 -0600

    feat(home): cleanup home page and header
    refs #2, #4

--- modules/events/client/events.menu.js
+++ modules/events/client/events.menu.js
@@ -6,26 +6,10 @@
     .run(runBlock);
 
   function runBlock(Menus) {
-
-    // Add the events dropdown item
     Menus.addMenuItem('topbar', {
       title: 'Events',
-      state: 'events',
-      type: 'dropdown',
+      state: 'events.list',
       roles: ['*']
-    });
-
-    // Add the dropdown list item
-    Menus.addSubMenuItem('topbar', 'events', {
-      title: 'List Events',
-      state: 'events.list'
-    });
-
-    // Add the dropdown create item
-    Menus.addSubMenuItem('topbar', 'events', {
-      title: 'Create Events',
-      state: 'events.create',
-      roles: ['user']
     });
   }
 })();
