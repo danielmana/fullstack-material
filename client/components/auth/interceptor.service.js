877e052318ee8ea99298fdbfad572f822c1a432f
Daniel Mañá Guixà <daniel81bcn@gmail.com>
2016-02-17 21:35:28 -0600

    feat(cordova): use headers on calls for both web and app

--- client/components/auth/interceptor.service.js
+++ client/components/auth/interceptor.service.js
@@ -2,42 +2,32 @@
 
 (function() {
 
-function authInterceptor($rootScope, $q, $cookies, $injector, Util, tokenService, deviceConfig) {
-  var state;
-  return {
-    // Add authorization token to headers
-    request(config) {
-      let token = tokenService.getToken();
-      if (!token) {
-        return config;
-      }
-      if (deviceConfig.usingDevice && !/\.html/.test(config.url)) {
-        // token for app
-        config.params = config.params || {};
-        _.extend(config.params, {
-          'access_token': token
-        });
-      } else {
-        // token for website
-        config.headers = config.headers || {};
-        config.headers.Authorization = 'Bearer ' + token;
-      }
-      return config;
-    },
+  function authInterceptor($q, $injector, tokenService) {
+    var state;
+    return {
+      // Add authorization token to headers
+      request(config) {
+          let token = tokenService.getToken();
+          if (token) {
+            config.headers = config.headers || {};
+            config.headers.Authorization = 'Bearer ' + token;
+          }
+          return config;
+        },
 
-    // Intercept 401s and redirect you to login
-    responseError(response) {
-      if (response.status === 401) {
-        (state || (state = $injector.get('$state'))).go('auth.login');
-        // remove any stale tokens
-        $cookies.remove('token');
-      }
-      return $q.reject(response);
-    }
-  };
-}
+        // Intercept 401s and redirect you to login
+        responseError(response) {
+          if (response.status === 401) {
+            (state || (state = $injector.get('$state'))).go('auth.login');
+            // remove any stale tokens
+            tokenService.removeToken();
+          }
+          return $q.reject(response);
+        }
+    };
+  }
 
-angular.module('kedb.component.auth')
-  .factory('authInterceptor', authInterceptor);
+  angular.module('kedb.component.auth')
+    .factory('authInterceptor', authInterceptor);
 
 })();
